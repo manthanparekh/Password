3515e61aacb68e0fe87fe3f8bd656f264fbbccf4
Manthan Parekh <manthan.parekh@outlook.com>
2023-10-24 18:05:21 -0400

    code

---
+++ src/Password.java
@@ -0,0 +1,16 @@
+
+// TODO: Create a Class
+public class Password {
+    public static void main(String[] args) {
+        // TODO: open a try statement
+        try {
+            String[] Passwrd = {"john", "1246", "odfiugh"};
+            System.out.println(Passwrd[90]);
+        }
+        // TODO: open a catch statement if try fails
+        catch (Exception e) {
+            System.out.println("input array value wrong");
+        }
+
+    }
+}
